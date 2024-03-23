521fcb2e781127de18ed3f6874de7e65ed27fd88
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-03-22 21:49:28 -0300

    test(questionService): adding question service test

---
+++ src/modules/questions/services/question.services.test.ts
@@ -0,0 +1,74 @@
+import getClient from "../../common/helpers/tests/getClient.ts";
+import QuizRepositoryFactory from "../../quiz/factories/quiz-repository.factory.ts";
+import QuestionRepositoryFactory from "../factories/question-repository.factory.ts";
+import setEnvs from "../../../config/env/setEnvs.ts";
+import { Quiz } from "../../quiz/types/quiz.types.ts";
+import { assert, assertRejects } from "../../../../deps/deps.ts";
+import QuestionService from "./question.service.ts";
+
+Deno.test("Question Service", async (t) => {
+  await setEnvs();
+  const client = await getClient();
+  const quizRepository = QuizRepositoryFactory.makeRepository(client);
+  const questionRepository = QuestionRepositoryFactory.makeRepository(client);
+  const questionService = new QuestionService(questionRepository);
+  const createdQuiz: Quiz = await quizRepository.create({
+    name: "Quiz",
+    subject: "Subject",
+  });
+
+  await t.step("It should insert a question correctly", async () => {
+    const createdQuestion = await questionService.createQuestion({
+      quiz_id: createdQuiz.id!,
+      title: "Question 1",
+    });
+
+    assert(createdQuestion);
+  });
+
+  await t.step("It should throw and exception when fields are missing", () => {
+    assertRejects(
+      async () => {
+        // @ts-expect-error - Testing if the exception is thrown
+        await questionService.createQuestion({
+          quiz_id: createdQuiz.id!,
+        });
+      },
+      "Error creating question",
+    );
+  });
+
+  await t.step("It should throw and exception when quiz_id is missing", () => {
+    assertRejects(
+      async () => {
+        // @ts-expect-error - Testing if the exception is thrown
+        await questionService.createQuestion({
+          title: "title",
+        });
+      },
+      "Error creating question",
+    );
+  });
+
+  await t.step("It should throw and exception when object is empty", () => {
+    assertRejects(
+      async () => {
+        // @ts-expect-error - Testing if the exception is thrown
+        await questionService.createQuestion({});
+      },
+      "Error creating question",
+    );
+  });
+
+  await t.step("It should throw and exception when param is null", () => {
+    assertRejects(
+      async () => {
+        // @ts-expect-error - Testing if the exception is thrown
+        await questionService.createQuestion(null);
+      },
+      "Error creating question",
+    );
+  });
+
+  client.end();
+});
